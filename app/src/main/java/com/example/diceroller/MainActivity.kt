7ee375892d1b48b47199a724c1175bc51593ff54
Dell <63410077@go.buu.ac.th>
2022-07-27 15:57:23 +0700

    it is my work!!

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        //val displayTextView: TextView = findViewById(R.id.textView)
+        rollButton.setOnClickListener {
+            //val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
+            //toast.show()
+            //displayText.text = "Sutanai"
+            rollDice()
+        }
+
+
+    }
+
+    class Dice(private val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView2)
+        val winToss: Button = findViewById(R.id.button)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+        if (diceRoll == 4){
+            val toast = Toast.makeText(this, "you win", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+    }
+}
+
